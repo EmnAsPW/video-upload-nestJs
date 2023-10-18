1a660bc0ddf3373161e2f03997cc99ef0ff756b8
EmnAsPW <emonsourov@gmail.com>
2023-10-18 11:52:59 +0600

    commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,8 +8,8 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({   
   imports: [    
-    // MongooseModule.forRoot("mongodb+srv://emonsourov:haCSbuZrobyBXent@videodb.m3rgdgm.mongodb.net/?retryWrites=true&w=majority"), VideoModule
-    MongooseModule.forRoot("mongodb://127.0.0.1:27017/newDb"), VideoModule
+    MongooseModule.forRoot("mongodb+srv://emonsourov:haCSbuZrobyBXent@videodb.m3rgdgm.mongodb.net/?retryWrites=true&w=majority"), VideoModule
+    // MongooseModule.forRoot("mongodb://127.0.0.1:27017/newDb"), VideoModule
   ], 
    
 })
